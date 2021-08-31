055985fff8468ba43630b06ea0a191fdc2db4e16
Mona Lisa <hridoyboss12@gmail.com>
2021-09-01 04:26:58 +0600

    added PWA

--- searchapp/settings.py
+++ searchapp/settings.py
@@ -46,12 +46,44 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'crispy_tailwind',
-    'django_cleanup.apps.CleanupConfig'
+    'django_cleanup.apps.CleanupConfig',
+    'pwa',
 ]
 
 # Crispy Form Settings
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
+
+# PWA Settings
+PWA_APP_NAME = 'Movie and TV'
+PWA_APP_DESCRIPTION = "Search for your favorite Movies and TV Shows"
+PWA_APP_THEME_COLOR = '#312e81'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/images/favicon.svg',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/images/favicon.svg',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/images/icons/splash-640x1136.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- searchapp/urls.py
+++ searchapp/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('searchmovies.urls')),
-    path('profile/', include('userprofile.urls'))
+    path('profile/', include('userprofile.urls')),
+    path('', include('pwa.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
