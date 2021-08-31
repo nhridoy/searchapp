fd35407a56f4ad0554d64c1a4cbbcc6affe2bb7f
Mona Lisa <hridoyboss12@gmail.com>
2021-09-01 03:24:37 +0600

    SSL Activated

--- searchapp/settings.py
+++ searchapp/settings.py
@@ -64,6 +64,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# SSL Activated
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'searchapp.urls'
 
 TEMPLATES = [
